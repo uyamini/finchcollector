bc8fa3b0fd9a991657096daaeb9a1e42366e8f6e
uyamini <uyamini1996@gmail.com>
2024-04-24 03:45:22 -0500

    finch-lab-3

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Finch
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -23,3 +23,15 @@ def finches_detail(request, finch_id):
     return render(request, 'finches/detail.html', {
         'finch': finch
     })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ('description', 'age')
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
